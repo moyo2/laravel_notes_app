5820068f1cc73d2aeaba84e9c2550d0d7dfe2ec4
Gift Moyo <giftmoyo23@gmail.com>
2024-06-23 00:50:56 +0200

    added_css

--- notes_book/resources/views/note/index.blade.php
+++ notes_book/resources/views/note/index.blade.php
@@ -1,17 +1,17 @@
 <x-layout>
 <div class="note-coontainer">
-     <a href="" class="new-note-btn">
+     <a href="{{Route('note.create')}}" class="new-note-btn">
           New Note
      </a>
      <div class="notes">
 @foreach ($notes as $note )
 <div class="note">
      <div class="note-body">
-          {{$note->note}}
+          {{Str::words($note->note, 30)}}
      </div>
      <div class="note-buttons">
-          <a href="" class="note-edit-button">View</a>
-          <a href="" class="note-edit-button">Edit</a>
+          <a href="{{Route('note.show', $note)}}" class="note-edit-button">View</a>
+          <a href="{{Route('note.edit', $note)}}" class="note-edit-button">Edit</a>
           <a href="" class="note-edit-button">Delete</a>
      </div>
 </div>
